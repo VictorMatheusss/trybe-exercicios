c930390002b3b92b95973e160d97a3ddcefa4607
Victor Santos <victormsantos142@gmail.com>
2023-05-04 15:10:34 -0300

    exercicio Arrays(Listas)

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/scripts.js
@@ -0,0 +1,21 @@
+// Exercicio 01
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// // Exercicio 02
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+// // Exercicio 03
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+// menu.push('Contatos')
+
+// console.log(menu);
