e420df021df819d92ac518ef3b76a901d7e0c883
Victor Santos <victormsantos142@gmail.com>
2023-05-04 13:20:37 -0300

    colocando tudo de novo

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/scripts.js
@@ -0,0 +1,14 @@
+let num1 = 7;
+let num2 = 22;
+
+let adicao = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+let modulo = num1 % num2;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
