cc89b6ff9a034d0fb514ea5769aaefa63fe2a42f
Victor Santos <victormsantos142@gmail.com>
2023-05-08 14:17:14 -0300

    Exercicio, agora a pratica

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/Exercicio01.js
@@ -0,0 +1,88 @@
+// Exercicio 01
+
+
+// function imprimeIdade() {
+//   for (let idade = 30; idade <= 40; idade += 1) {
+//     console.log('Idade dentro do for:', idade)
+//   }
+// }
+// imprimeIdade()
+
+
+  // Exercicio 02
+  // const pessoa = {
+  //   nome: 'Henri',
+  //   idade: 20
+  // }
+  // pessoa.nome = {
+  //   nome: 'Luna',
+
+  // } 
+  // pessoa.idade = {
+  //   idade: 19
+  // }
+  // console.log('Nome:', pessoa.nome);
+  // console.log('Idade:', pessoa.idade);
+
+
+  // Exercicio 03
+  
+  
+  // let favoriteFood = 'Lasanha';
+
+  // favoriteFood = 'Hambúrguer';
+
+  // console.log(favoriteFood);
+
+
+  // Exercicio 04
+
+  // const name = 'Adriana';
+  // const lastName = 'Soares';
+  // console.log(`Olá, ${name} ${lastName}!`);
+
+  // let a = 3;
+  // let b = 5;
+  // function soma(a, b) {
+  //   let resultado = a + b;
+  //   return resultado;
+  // }
+  
+  
+  // // console.log('O resultado da soma de ' + a + ' + ' + b + ' é: ' + soma(a,b));
+  // console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a, b)}`);
+ 
+
+// Exercicio 05
+
+
+// const numeroAleatorio = Number => Number * 2;
+
+// console.log(numeroAleatorio(30));
+
+// Exercicio 06
+
+// const nome = () => 'Victor'
+
+// console.log(`Olá, ${nome()}`);
+
+
+// Exercicio 07
+
+
+// const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`;
+// let nome = 'Ivan';
+// let sobrenome = 'Pires';
+
+// console.log(`Seja bem vindo, ${nomeCompleto(nome, sobrenome)}`);
+
+
+
+// Exercicio 08
+
+
+// let speed = 130;
+
+// const speedCar = (speed >= 120) ? "Você ultrapassou o limite de velocidade" : "Você está na velocidade permitida"
+
+// console.log(speedCar);
